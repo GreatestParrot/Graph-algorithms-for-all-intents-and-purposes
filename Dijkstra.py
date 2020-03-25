f21ec07bc02a0b15477e230faed123cc0ad77260
Arseniy Popov <44940020+arseniypopugay@users.noreply.github.com>
2020-03-25 22:45:01 +0300

    Add files via upload
    First and my most unliked one

---
+++ Dijkstra.py
@@ -0,0 +1,46 @@
+# Need Start vertex number and Graph
+# Graph structure:
+# Grap = list of vetrexes
+# Graph[vertex]=list of neighbour_vertexes
+# neighbour_ver = list:
+#     neighbour_ver[0] - number of vertex
+#     neighbour_ver[1] - distanse frome origin_vertex to neighbour`
+# Also required inf - "number that could be thought about as infinity"
+
+def Dijkstra(Graph, start, inf):
+
+    dist = list()
+    prev = list()
+    vertex = set()
+    for i in range(len(Graph)):
+        dist.append(inf)
+        prev.append(-1)
+        if len(Graph[i])!=0:
+            vertex.add(i)
+
+    dist[start]=0
+
+    while (len(vertex)>0):
+
+        u = vertex.pop()
+        vertex.add(u)
+
+    
+
+        for i in vertex:
+            if dist[i]<dist[u]:
+                u=i
+    
+        vertex.remove(u)
+
+        # print(str(u)+ "  " + str(dist[u]) + "  "+ str(Graph[u]))
+
+        for neighbour_ver in Graph[u]:
+            alt = dist[u]+neighbour_ver[1]
+            if alt < dist[neighbour_ver[0]]:
+                dist[neighbour_ver[0]] = alt
+                prev[neighbour_ver[0]] = u
+
+#return dist
+    return dist, prev
+
